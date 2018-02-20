bb82b5429fb43372977727d4fbec0010cf13ca90
awadhoot.lele <awadhoot.lele@synerzip.com>
2018-02-20 16:20:11 +0530

    added simple onChange event on search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react';
 
 export default class SearchBar extends React.Component {
+  test = '';
   render() {
-    return (<input/>)
+    return (
+      <input onChange={this.onInputChange}/>
+    )
+  }
+
+  onInputChange(e) {
+    console.log('Input changed -> ', e);
   }
 }
