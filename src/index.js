98f84301a26a11edb8909a6f36db3979be00aa4e
awadhoot.lele <awadhoot.lele@synerzip.com>
2018-02-20 17:04:25 +0530

    consuming youtube api and some refactoring

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 
 const API_KEY = 'AIzaSyDwDFVyefEUmSFkf639Sc7d7W5kUKN9ob0';
-const App = () => <div><SearchBar/></div>;
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {videos: []};
+    YTSearch({
+      key: API_KEY,
+      term: 'neistat'
+    }, (videos) => {
+      console.log(data);
+      this.setState({...this.state, videos});
+    });
+
+  }
+
+  render() {
+    return (
+      <div><SearchBar/></div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
