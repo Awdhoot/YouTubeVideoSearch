bc5f120437d4e3403dc56326ca50157bf80e7cac
awadhoot.lele <awadhoot.lele@synerzip.com>
2018-02-21 11:43:05 +0530

    added callbacks as props

--- src/index.js
+++ src/index.js
@@ -11,12 +11,12 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {videos: []};
+    this.state = {videos: [], selectedVideo: null};
     YTSearch({
       key: API_KEY,
-      term: 'neistat'
+      term: 'MKBHD'
     }, (videos) => {
-      this.setState({...this.state, videos});
+      this.setState({...this.state, videos, selectedVideo: videos[0]});
     });
   }
 
@@ -24,8 +24,12 @@ class App extends Component {
     return (
       <div>
         <SearchBar/>
-        <VideoDetail video={this.state.videos[0]}/>
-        <VideoList videos={this.state.videos}/>
+        <VideoDetail
+          video={this.state.selectedVideo}
+        />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({...this.state, selectedVideo})}
+          videos={this.state.videos}/>
       </div>
     );
   }
